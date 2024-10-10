83a651e40602618d2eff896b6e42da293550f52b
Fake Dude <fakedude@gmail.com>
2024-10-11 01:28:31 +0400

    refactor: remove bypass-node service and update SSL file paths in config

--- config.py
+++ config.py
@@ -3,16 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 SERVICE_PORT = config('SERVICE_PORT', cast=int, default=62050)
-
 XRAY_API_PORT = config('XRAY_API_PORT', cast=int, default=62051)
 XRAY_EXECUTABLE_PATH = config("XRAY_EXECUTABLE_PATH", default="/usr/local/bin/xray")
 XRAY_ASSETS_PATH = config("XRAY_ASSETS_PATH", default="/usr/local/share/xray")
 
-SSL_CERT_FILE = config("SSL_CERT_FILE", default="/var/lib/bypass-node/ssl_cert.pem")
-SSL_KEY_FILE = config("SSL_KEY_FILE", default="/var/lib/bypass-node/ssl_key.pem")
-SSL_CLIENT_CERT_FILE = config("SSL_CLIENT_CERT_FILE", default="")
+SSL_CERT_FILE = config("SSL_CERT_FILE", default="/var/lib/marzban-node/ssl_cert.pem")
+SSL_KEY_FILE = config("SSL_KEY_FILE", default="/var/lib/marzban-node/ssl_key.pem")
+SSL_CLIENT_CERT_FILE = config("SSL_CLIENT_CERT_FILE", default="/var/lib/marzban-node/ssl_client_cert.pem")
 
 DEBUG = config("DEBUG", cast=bool, default=False)
 
